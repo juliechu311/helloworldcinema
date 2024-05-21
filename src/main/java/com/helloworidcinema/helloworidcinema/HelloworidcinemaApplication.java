1ce5d28d4c19167f60e4a3e9a3a46af9f0afd3d7
喬喬 <juliechu311@gmail.com>
2024-05-21 15:11:19 +0800

    初始化

---
+++ src/main/java/com/helloworidcinema/helloworidcinema/HelloworidcinemaApplication.java
@@ -0,0 +1,13 @@
+package com.helloworidcinema.helloworidcinema;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloworidcinemaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelloworidcinemaApplication.class, args);
+	}
+
+}
